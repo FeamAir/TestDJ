d9adb2bf26df8e5b144238f3d526e247c0d41119
Feam <maryking.textile@gmail.com>
2022-06-13 11:47:33 +0300

    add test positiv and negativ values

--- src/main/java/com/hillel/lesson15/Calculator.java
+++ src/main/java/com/hillel/lesson15/Calculator.java
@@ -1,13 +1,7 @@
 package com.hillel.lesson15;
 
 
-
 public class Calculator {
-    public void main(String[] args) {
-        Double t = calculate(4,"+",2);
-        System.out.println(t);
-    }
-
 
     public Double calculate(double a, String operation, double b) {
         Operation value = Operation.convert(operation);
@@ -18,17 +12,32 @@ public class Calculator {
                 || operation.equalsIgnoreCase("/")) {
             switch (value) {
                 case ADD:
-                    return Operation.add(a, b);
+                    if (a < 0 || b < 0) {
+                        return Operation.add(a, b);
+                    } else {
+                        return Operation.add(a, b);
+                    }
                 case SUB:
-                    return Operation.sub(a, b);
+                    if (a < 0 || b < 0) {
+                        return Operation.sub(a, b);
+                    } else {
+                        return Operation.sub(a, b);
+                    }
                 case MUL:
-                    return Operation.mul(a, b);
+                    if (a < 0 || b < 0) {
+                        return Operation.mul(a, b);
+                    } else {
+                        return Operation.mul(a, b);
+                    }
                 case DIV:
                     if (a == 0 || b == 0) {
                         System.out.println("Divide by 0");
                         return -1.0;
+                    } else if (a < 0 || b < 0) {
+                        return Operation.div(a, b);
+                    } else if (a > 0 || b > 0) {
+                        return Operation.div(a, b);
                     }
-                    return Operation.div(a, b);
             }
         }
         return -1.0;
--- src/test/java/com/hillel/lesson15/CalculatorTest.java
+++ src/test/java/com/hillel/lesson15/CalculatorTest.java
@@ -3,35 +3,38 @@ package com.hillel.lesson15;
 import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
-public class CalculatorTest extends TestCase {
+public class CalculatorTest {
     Calculator calculator;
-    private final double a = 4.0;
-    private final double b = 2.0;
 
     @Before
     public void setUp() throws Exception {
         calculator = new Calculator();
     }
 
+    @Test
     public void testMain() {
     }
-
+    @Test
     public void testCalculateAdd() {
-        double actual = calculator.calculate(a, "+", b);
-        Assert.assertEquals(6.0, actual, 0.0);
+        double actual = calculator.calculate(-5, "+", 0);
+        Assert.assertEquals(-5, actual, 0.0);
     }
+    @Test
     public void testCalculateSub() {
-        double actual = calculator.calculate(a, "-", b);
+        double actual = calculator.calculate(2, "-", 4);
         Assert.assertEquals(2.0, actual, 0.0);
     }
+    @Test
     public void testCalculateMul() {
-        double actual = calculator.calculate(a, "*", b);
+        double actual = calculator.calculate(2, "*", 4);
         Assert.assertEquals(8.0, actual, 0.0);
     }
+    @Test
     public void testCalculateDiv() {
-        double actual = calculator.calculate(a, "/", b);
-        Assert.assertEquals(2.0, actual, 0.0);
+        double actual = calculator.calculate(-2, "/", 2);
+        Assert.assertEquals(-1.0, actual, 0.0);
 
     }
 }
